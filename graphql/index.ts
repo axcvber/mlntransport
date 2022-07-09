3a8737941e1fbad6dbdc5fec3866a3e053913784
Alex <axlisovsky@gmail.com>
2022-07-10 00:18:10 +0300

    gql

--- graphql/index.ts
+++ graphql/index.ts
@@ -17,7 +17,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
-  uri: `${process.env.SERVER_URL}/graphql`,
+  uri: `https://mlntransport-admin.com/graphql`,
 })
 const client = new ApolloClient({
   cache,

df047b9ad241287c3ddc5cdc1c07f2f744b55c35
Alex <axlisovsky@gmail.com>
2022-07-15 19:08:41 +0300

    cache off

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -20,8 +20,8 @@ const Page: React.FC<IPage> = ({ pageData }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { res } = context
-  res.setHeader('Cache-Control', 'public, s-maxage=43200, stale-while-revalidate=60')
+  // const { res } = context
+  // res.setHeader('Cache-Control', 'public, s-maxage=43200, stale-while-revalidate=60')
 
   const { locale } = context
   const { slug } = context.params as any
